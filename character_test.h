0feabb67379656d9de435be7257ffc9032141f38
Leoreo <marcos@villastenerife.com>
2024-03-04 11:00:24 +0100

    character_test.h sin documentar

---
+++ character_test.h
@@ -0,0 +1,36 @@
+/** 
+ * @brief It declares the tests for the character module
+ * 
+ * @file character_test.h
+ * @author Diego Tabero & Marcos Leo Sonck
+ */
+
+#ifndef CHARACTER_TEST_H
+#define CHARACTER_TEST_H
+
+
+void test1_character_create();
+void test2_character_create();
+void test1_character_destroy();
+void test2_character_destroy();
+void test1_character_get_id();
+void test2_character_get_id();
+void test1_character_set_name();
+void test2_character_set_name();
+void test1_character_get_name();
+void test2_character_get_name();
+void test1_character_set_hp();
+void test2_character_set_hp();
+void test1_character_get_hp();
+void test2_character_get_hp();
+void test1_character_set_message();
+void test2_character_set_message();
+void test1_character_get_message();
+void test2_character_get_message();
+void test1_character_set_friendly();
+void test2_character_set_friendly();
+void test1_character_get_friendly();
+void test2_character_get_friendly();
+
+
+#endif
